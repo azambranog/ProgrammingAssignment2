91d5c1df3ac870ef42708a5b4ca0407fd9bab991
andres <ndrszmbrn@gmail.com>
2014-04-23 19:07:25 +0200

    this is my solution for the programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We use the <<- operator to create two functions that 
+## create an object that stores a matrux and cache's 
+## its inverse.
 
-## Write a short comment describing this function
+
+
+## This function creates a matrix object which is a list
+## that have functions for set and get the value of the 
+## matrix and to get and set the value of its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
+  set <- function(y) { 
+    #change value on the matrix and make inverse NULL
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x  #get the value of the matrix
+  setinverse <- function(inv) inverse <<- inv #get inverse
+  getinverse <- function() inverse #set inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
+## this function calcualtes the inverse of a matrix
+##created by the makeCacheMatrix function.
+## If the inverse has been calculated and the matrix
+## has not changed, then the calculation is skipped, 
+## this is achieved because we set the inverse to null
+## in x.set(), so, every time the matrix changes, then
+## the inverse is set to NULL
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()#get the current inverse
+  if(!is.null(inverse)) { 
+    #if the inverse have been set, then get if from cahe
+    message("getting cached data")
+    return(inverse)
+  }
+  normal <- x$get()
+  inverse <- solve(normal, ...)
+  x$setinverse(inverse)
+  inverse
 }
